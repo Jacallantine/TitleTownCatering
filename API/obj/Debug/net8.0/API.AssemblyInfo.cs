fed141db34f8953c0fc6964fca66c4ff2edd24fe
Steven <sgschneider@crimson.ua.edu>
2024-11-20 14:00:07 -0600

    merged

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c64678648028975c26225a8a53ddd2fadbeac3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+912346023935af426662e066a3e70dc6c46b59e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
