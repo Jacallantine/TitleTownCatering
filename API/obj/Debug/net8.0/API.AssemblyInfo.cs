cb082c1fc1ef18fd1380b00a132a4a1a850b2fda
Jared <jacallantine@crimson.ua.edu>
2024-11-21 01:55:43 -0600

    jacallantine

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba0adbf7351b1f82d38c0e5dc4eb501b7fc48479")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af3ee191c74cd8500e38375913faf8cad4eec27")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
