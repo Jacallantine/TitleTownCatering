6518aed7e2c09d6495fd852c9d14e8a82eb03113
amvanalstyne <amvanalstyne@crimson.ua.edu>
2024-11-24 17:47:47 -0500

    amvanalstyne 11/24 menu quantity

--- CLIENT/JS/menu.js
+++ CLIENT/JS/menu.js
@@ -11,60 +11,60 @@ function fillMenu(){
 function Breakfast(){
     let html = `
     <ul>
-    <li>Biscuit 
-    <span>$50</span></li>
-    <li>Sausage Egg and Cheese Biscuit 
-    <span>$60</span></li>
-    <li>Sausage Biscuit 
-    <span>$55</span></li>
-    <li>Egg and Cheese Biscuit 
-    <span>$55</span></li>
+    <li>Biscuits <span class="quantity">Feeds 20</span>
+    <span class="price">$50</span></li>
+    <li>Sausage Egg and Cheese Biscuits <span class="quantity">Feeds 20</span>
+    <span class="price">$60</span></li>
+    <li>Sausage Biscuits <span class="quantity">Feeds 20</span>
+    <span class="price">$55</span></li>
+    <li>Egg and Cheese Biscuits <span class="quantity">Feeds 20</span> 
+    <span class="price">$55</span></li>
   </ul>`
     document.getElementById("breakfast").innerHTML = html;
 }
 function Lunch(){
     let html = `<ul>
-    <li>Chicken Sandwich
-    <span>$50</span></li>
-    <li>Hot Dogs
-    <span>$40</span></li>
-    <li>Burgers 
-    <span>$45</span></li>
-    <li>Po' Boys 
-    <span>$45</span></li>
+    <li>Chicken Sandwiches <span class="quantity">Feeds 20</span>
+    <span class="price">$50</span></li>
+    <li>Hot Dogs <span class="quantity">Feeds 20</span>
+    <span class="price">$40</span></li>
+    <li>Burgers <span class="quantity">Feeds 20</span> 
+    <span class="price">$45</span></li>
+    <li>Po' Boys <span class="quantity">Feeds 20</span> 
+    <span class="price">$45</span></li>
   </ul>`
     document.getElementById("lunch").innerHTML = html;
 }
 function Dinner(){
     let html = `<ul>
-    <li>Steak 
-    <span>$100</span></li>
-    <li>Fried Catfish 
-    <span>$65</span></li>
-    <li>Low Country Boil
-    <span>$65</span></li>
+    <li>Steaks <span class="quantity">Feeds 20</span> 
+    <span class="price">$100</span></li>
+    <li>Fried Catfishes <span class="quantity">Feeds 20</span> 
+    <span class="price">$65</span></li>
+    <li>Low Country Boils <span class="quantity">Feeds 20</span>
+    <span class="price">$65</span></li>
     </ul>`
     document.getElementById("dinner").innerHTML = html;
 }
 function Sides(){
     let html = `<ul>
-    <li>Mac and Cheese 
-    <span>$20</span></li>
-    <li>Mashed Potatoes 
-    <span>$20</span></li>
-    <li>Cheese and Grits
-    <span>$20</span></li>
+    <li>Mac and Cheese <span class="quantity">Feeds 20</span> 
+    <span class="price">$20</span></li>
+    <li>Mashed Potatoes <span class="quantity">Feeds 20</span> 
+    <span class="price">$20</span></li>
+    <li>Cheese and Grits <span class="quantity">Feeds 20</span>
+    <span class="price">$20</span></li>
     </ul>`
     document.getElementById("sides").innerHTML = html;
 }
 function Drinks(){
     let html = `<ul>
-    <li>Sweet Tea 
-    <span>$15</span></li>
-    <li>Coca Cola 
-    <span>$15</span></li>
-    <li>Lemonade
-    <span>$15</span></li>
+    <li>Sweet Tea <span class="quantity">Feeds 20</span> 
+    <span class="price">$15</span></li>
+    <li>Coca Cola <span class="quantity">Feeds 20</span> 
+    <span class="price">$15</span></li>
+    <li>Lemonade <span class="quantity">Feeds 20</span>
+    <span class="price">$15</span></li>
     </ul>`
     document.getElementById("drinks").innerHTML = html;
 }
